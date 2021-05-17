c540d7c4d08c82b7860985ac7ca2458c0b23c17c
ming-ki <wbakswl010@naver.com>
2021-05-17 16:36:57 +0900

    commit

---
+++ Homework/MiroQueue.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+#define MAZE_SIZE 7
+
+typedef struct {
+	int row;
+	int col;
+}element;
+
+typedef struct {
+	int front;
+	int rear;
+	element data[MAX_QUEUE_SIZE];
+}QueueType;
+
+element here = { 1.0 };
+
+char maze[MAZE_SIZE][MAZE_SIZE] = { {'1','1','1','1','1','1','1'},{'0','0','1','0','0','0','0'},  {'1','0','0','0','1','0','1'},  {'1','0','1','0','1','0','1'},  {'0','0','1','1','0','0','1'},  {'1','0','1','0','0','1','x'},  {'1','0','1','1','0','0','0'} };
+
+void error(char* message)
+{
+	fprintf(stderr, "%s\n", message);
+}
+
+void init_queue(QueueType* q)
+{
+	q->rear = 0;
+	q->front = 0;
+}
+
+int is_empty(QueueType* q)
+{
+	return(q->front==q->rear);
+}
+
+int is_full(QueueType* q)
+{
+	return (q->front == (q->rear + 1) % MAX_QUEUE_SIZE);
+}
+
+void enqueue(QueueType* q, element item)
+{
+	if (is_full(q))
+	{
+		error("큐가 포화상태입니다.");
+		return;
+	}
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType* q)
+{
+	if (is_empty(q))
+	{
+		error("큐가 공백상태입니다.");
+		exit(1);
+	}
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+void pushLocation(QueueType* q, int row, int col)
+{
+	if (row < 0 || row >= MAZE_SIZE || col < 0 || col >= MAZE_SIZE)
+	{
+		return;
+	}
+	if (maze[row][col] != '1' && maze[row][col] != '.')
+	{
+		element temp;
+		temp.row = row;
+		temp.col = col;
+		enqueue(q, temp);
+	}
+}
+
+void printMaze()
+{
+	system("cls");
+
+	for (int i = 0; i < MAZE_SIZE; i++)
+	{
+		for (int j = 0; j < MAZE_SIZE; j++)
+		{
+			printf("%c ", maze[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int r, c;
+	QueueType q;
+	init_queue(&q);
+	maze[here.row][here.col] = 'e';
+	printMaze();
+	getchar();
+
+	while (maze[here.row][here.col] != 'x')
+	{
+		r = here.row;
+		c = here.col;
+		maze[r][c] = 'm';
+		printMaze();
+		getchar();
+
+		maze[r][c] = '.';
+		pushLocation(&q, r - 1, c);
+		pushLocation(&q, r + 1, c);
+		pushLocation(&q, r, c);
+		pushLocation(&q, r, c + 1);
+
+		if (is_empty(&q))
+		{
+			printf("실패\n");
+			return 0;
+		}
+		else here = dequeue(&q);
+	}
+	maze[here.row][here.col] = 'm';
+	printMaze();
+	printf("성공\n");
+}
---
+++ Homework/MiroStack.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK_SIZE 100
+#define MAZE_SIZE 7
+
+typedef struct {
+	int row;
+	int col;
+}element;
+
+typedef struct {
+	element data[MAX_STACK_SIZE];
+	int top;
+}StackType;
+
+element here = { 1.0 };
+
+char maze[MAZE_SIZE][MAZE_SIZE] = { {'1','1','1','1','1','1','1'},{'0','0','1','0','0','0','0'},  {'1','0','0','0','1','0','1'},  {'1','0','1','0','1','0','1'},  {'0','0','1','1','0','0','1'},  {'1','0','1','0','0','1','x'},  {'1','0','1','1','0','0','0'}};
+
+void init_stack(StackType* s)
+{
+	s->top = -1;
+}
+
+int is_empty(StackType* s)
+{
+	return(s->top == -1);
+}
+
+int is_full(StackType* s)
+{
+	return (s->top == (MAX_STACK_SIZE)-1);
+}
+
+void push(StackType* s, element item)
+{
+	if (is_full(s))
+	{
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+	}
+	else s->data[++(s->top)] = item;
+}
+
+element pop(StackType* s)
+{
+	if (is_empty(s))
+	{
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[(s->top)--];
+}
+
+element peek(StackType* s)
+{
+	if (is_empty(s))
+	{
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[s->top];
+}
+
+void pushLocation(StackType* s, int row, int col)
+{
+	if (row < 0 || row >= MAZE_SIZE || col < 0 || col >= MAZE_SIZE)
+	{
+		return;
+	}
+	if (maze[row][col] != '1' && maze[row][col] != '.')
+	{
+		element temp;
+		temp.row = row;
+		temp.col = col;
+		push(s, temp);
+	}
+}
+
+void printMaze()
+{
+	system("cls");
+
+	for (int i = 0; i < MAZE_SIZE; i++)
+	{
+		for (int j = 0; j < MAZE_SIZE; j++)
+		{
+			printf("%c ", maze[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int r, c;
+	StackType s;
+	init_stack(&s);
+	maze[here.row][here.col] = 'e';
+	printMaze();
+	getchar();
+
+	while (maze[here.row][here.col] != 'x')
+	{
+		r = here.row;
+		c = here.col;
+		maze[r][c] = 'm';
+		printMaze();
+		getchar();
+
+		maze[r][c] = '.';
+		pushLocation(&s, r - 1, c);
+		pushLocation(&s, r + 1, c);
+		pushLocation(&s, r, c);
+		pushLocation(&s, r, c + 1);
+
+		if (is_empty(&s))
+		{
+			printf("실패\n");
+			return 0;
+		}
+		else here = pop(&s);
+	}
+	maze[here.row][here.col] = 'm';
+	printMaze();
+	printf("성공\n");
+}
